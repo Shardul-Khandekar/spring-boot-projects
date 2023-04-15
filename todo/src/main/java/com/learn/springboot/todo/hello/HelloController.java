1bb0d5a49240ea22833a4e614bf6c0b42e919fe2
Shardul Khandekar <khandekarshardul@gmail.com>
2023-04-15 12:34:40 +0530

    Rendering JSP pages

--- todo/src/main/java/com/learn/springboot/todo/hello/HelloController.java
+++ todo/src/main/java/com/learn/springboot/todo/hello/HelloController.java
@@ -1,21 +1,30 @@
 package com.learn.springboot.todo.hello;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HelloController {
 
 	@RequestMapping("/hello")
+	@ResponseBody
 	public String hello() {
 		return "Hello, World!";
 	}
 
 	@RequestMapping("/hello-html")
+	@ResponseBody
 	public String hellohtml() {
 
 		StringBuffer sb = new StringBuffer();
 		sb.append("<html><head><title>Hello HTML</title></head><body><p>Rendering HTML</p></body></html>");
 		return sb.toString();
 	}
+
+	@RequestMapping("/hello-jsp")
+	public String hellojsp() {
+		return "hello";
+	}
+
 }
---
+++ todo/src/main/resources/META-INF/resources/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>Hello JSP</title>
+	</head>
+	<body>
+		<p>Rendering HTML from JSP page</p>
+	</body>
+</html>
