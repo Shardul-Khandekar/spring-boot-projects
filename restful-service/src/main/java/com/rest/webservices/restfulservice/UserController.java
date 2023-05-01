b7788f682d166ce8c80cbe63317b9f16a271b03d
Shardul Khandekar <khandekarshardul@gmail.com>
2023-05-01 11:54:10 +0530

    Enhancing the add function to read the object from the request body

--- restful-service/src/main/java/com/rest/webservices/restfulservice/UserController.java
+++ restful-service/src/main/java/com/rest/webservices/restfulservice/UserController.java
@@ -1,10 +1,12 @@
 package com.rest.webservices.restfulservice;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +19,20 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
-	@PostMapping("/users/add")
-	public String addUser(@RequestParam String name, @RequestParam String email) {
+//	@PostMapping("/users/add")
+//	public String addUser(@RequestParam String name, @RequestParam String email) {
+//
+//		User user = new User();
+//		user.setName(name);
+//		user.setEmail(email);
+//		userRepository.save(user);
+//
+//		return "User Added!";
+//	}
 
-		User user = new User();
-		user.setName(name);
-		user.setEmail(email);
+	@PostMapping("/users/add")
+	public String addUser(@RequestBody User user) {
+
 		userRepository.save(user);
 
 		return "User Added!";
@@ -32,6 +42,13 @@ public class UserController {
 	public Iterable<User> getAllUsers() {
 
 		return userRepository.findAll();
+	}
+
+	@GetMapping("/users")
+	public Optional<User> getUserDetails(@RequestParam Integer id) {
+
+		return userRepository.findById(id);
+
 	}
 
 }
